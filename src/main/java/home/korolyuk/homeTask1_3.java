6fc063114766d35fb5a2ddbc3ff5c7d388d7c626
wolfchkov <wolfchkovaj@gmail.com>
2018-02-07 21:50:11 +0200

    Update homeTask1_3.java
    мелкие замечания

--- src/main/java/home/korolyuk/homeTask1_3.java
+++ src/main/java/home/korolyuk/homeTask1_3.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 /**
  * Created by admin on 2/4/2018.
  */
+//Имя класса с большой буквы
 public class homeTask1_3 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
