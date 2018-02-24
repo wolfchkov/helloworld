d352e4f4686967db66ea599b0997eb2305af8c58
wolfchkov <wolfchkovaj@gmail.com>
2018-02-24 22:14:29 +0200

    Update HomeTask3_2.java
    замечания

--- src/main/java/home/korolyuk/HomeTask3_2.java
+++ src/main/java/home/korolyuk/HomeTask3_2.java
@@ -11,6 +11,13 @@ public class HomeTask3_2 {
     public static StringBuilder strsb = new StringBuilder();
     public static Scanner scan = new Scanner(System.in);
 
+    //Решение принимается, но не очень оптимальное, так как цикл в цикле
+    //можно решить использую всего лишь один цикл
+    //там только латинские буквы а они лежат в диапазоне чисел 0 - 128
+    //достаточно создать массив, int[128] и использовать сивол
+    //как индекс в этом массиве, прибавляя каждый раз еденичку в элементе массива
+    //индекс которого и является буквой :)
+    //только нужно проверить, что пользователь ввел только латинские буквы
     public static void symbolCounter(){
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
@@ -30,6 +37,9 @@ public class HomeTask3_2 {
 
     public static void main(String[] args) {
         System.out.println("Введите строку");
+        //не очень хорошая идея использовать статическое поле как переменную 
+        //для чтения строки, лучше объявите тут переменную а 
+        //в метод добавте параметр symbolCounter(String str)
         str = scan.nextLine();
         symbolCounter();
     }
