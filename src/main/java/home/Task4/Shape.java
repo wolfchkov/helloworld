714d2c550c6ba3ef13d5c2197388235927c04b9c
wolfchkov <wolfchkovaj@gmail.com>
2018-03-04 22:38:23 +0200

    Update Shape.java

--- src/main/java/home/Task4/Shape.java
+++ src/main/java/home/Task4/Shape.java
@@ -1,13 +1,17 @@
 package home.Task4;
 
 public abstract class Shape {
+    //Эти поля не нужны, расчет в самом методе 
     public float square;
     public float perimeter;
 
+    //метод должен быть абстрактным а его реализация в конкретной фигуре
     public float getSquare(){
         System.out.println("Площадь = " + square);
         return square;
     }
+
+    //метод должен быть абстрактным а его реализация в конкретной фигуре
     public float getPerimeter() {
         System.out.println("периметр = " + perimeter);
         return perimeter;
